ab753a843dde9310ce80322138469b8ec0b2f04c
heathermkrause <heather@idatassist.com>
2016-08-18 15:13:52 -0400

    Recoding 2001 variables to match with 2006 and 2011

--- Aboriginal Census Data.R
+++ Aboriginal Census Data.R
@@ -14,6 +14,63 @@ data2001$Year <- 2001
 data2011$Year <- 2011
 data2006$Year <- 2006
 
+data2001$pr <- data2001$provp
+
+data2001$hhtype <- data2001$htypep
+
+data2001$cfsize <- data2001$cfsizep
+
+data2001$attsch <- data2001$schattp
+
+data2001$cip2000 <- data2001$dgmfsp
+
+data2001$mob1 <- data2001$mob1p
+
+data2001$mob5 <- data2001$mob5p
+
+data2001$pr1 <- data2001$prov1p
+
+data2001$pr5 <- data2001$prov5p
+
+data2001$dist <- data2001$distp
+
+data2001$mode <- data2001$modep
+
+data2001$lfact <- data2001$lfactp
+
+data2001$fptwk <- data2001$fptwkp
+
+data2001$lstwrk <- data2001$lstwkp
+
+data2001$wkswrk <- data2001$wkswkp
+
+data2001$naics <- data2001$naicsp
+
+data2001$nocs <- data2001$nocs01p
+
+data2001$totinc <- data2001$totincp
+
+data2001$gtrfs <- data2001$tgovtp
+
+data2001$hhinc <- data2001$hhincp
+
+data2001$weight <- data2001$weightp
+
+data2001$pkids <- data2001$prescp
+
+data2001$immstat <- data2001$immpopp
+
+data2001$yrimm <- data2001$yrimmig
+
+
+
+data2001$aboid[data2001$absrp==1] <- 6
+data2001$aboid[data2001$absrp==2] <- 1
+data2001$aboid[data2001$absrp==3] <- 2
+data2001$aboid[data2001$absrp==4] <- 3
+data2001$aboid[data2001$absrp==5] <- 4
+data2001$aboid[data2001$absrp==6] <- 5
+
 names(data2011)
 summary(data2011$aboid)
 tab(data2011$aboid)
@@ -26,3 +83,10 @@ summary(dataM2$Year)
 summary(dataS$Year)
 length(dataM2$Year)
 length(dataS$Year)
+
+
+length(dataS$aboid)
+length(data2001$aboid)
+length(data2006$aboid)
+length(data2011$aboid)
+
